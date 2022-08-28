d6213567fb906520e541f9c492105c3442fa8454
Monotosh Das <monotosh.das@videonetics.com>
2022-08-28 08:32:54 +0000

    Copied from Walnut

---
+++ Walnut/src/Walnut/Input/Input.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "KeyCodes.h"
+
+#include <glm/glm.hpp>
+
+namespace Walnut
+{
+
+class Input
+{
+public:
+  static bool IsKeyDown(KeyCode keycode);
+  static bool IsMouseButtonDown(MouseButton button);
+
+  static glm::vec2 GetMousePosition();
+
+  static void SetCursorMode(CursorMode mode);
+};
+
+} // namespace Walnut
